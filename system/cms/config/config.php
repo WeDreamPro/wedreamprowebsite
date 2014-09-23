b893c704419f5650986cb133bde10bb07d3c6711
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-09-23 09:13:24 -0500

    Git ignore and instalation

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -230,7 +230,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'gcVS/WNNo4qe6Z7XXd3/K+0MEMoHhh8e6LLL5hZB77';
 
 /*
 |--------------------------------------------------------------------------
