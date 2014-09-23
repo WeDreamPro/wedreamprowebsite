bf5b7268ccf86f3937977bc053941cb01f8d6e45
Jose Luis Fonseca <desarrollo@reddesignsystems.com>
2014-09-23 09:57:51 -0500

    Home page and theme

---
+++ addons/shared_addons/themes/wedreampro/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Wedreampro extends Theme {
+
+    public $name = 'We dream Pro';
+    public $author = 'Jose Fonseca';
+    public $author_website = 'http://www.josefonseca.me/';
+    public $website = 'http://www.wedreampro.com/';
+    public $description = 'We dream pro template';
+    public $version = '1.0.0';
+
+}
+
+/* End of file theme.php */
